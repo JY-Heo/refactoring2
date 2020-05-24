3e0f5f5f5233783522d356d0266addea91f5c390
JY Heo <jirung.h@gmail.com>
2020-05-24 19:26:22 +0900

    1.8/amountFor도 계산기를 이용하도록 수정

--- chapter01/1.8 다형성을 활용해 계산 코드 재구성하기/createStatementData.js
+++ chapter01/1.8 다형성을 활용해 계산 코드 재구성하기/createStatementData.js
@@ -50,25 +50,7 @@ module.exports = function createStatementData(invoice, plays) { // 중간 데이
     }
 
     function amountFor(aPerformance) { // 값이 바뀌지 않는 변수는 매개변수로 전달
-        let result = 0; // 변수를 초기화하는 코드
-        switch (aPerformance.play.type) {
-            case 'tragedy':
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case 'comedy':
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-        }
-        return result; // 함수 안에서 값이 바뀌는 변수 반환
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount; // amountFor()도 계산기를 이용하도록 수정
     }
 
     function volumeCreditsFor(aPerformance) {
