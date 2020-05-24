d9b58a733e24054bf312314999a220964a4b74c2
JY Heo <jirung.h@gmail.com>
2020-05-24 19:10:49 +0900

    1.6/html 버전 작성

--- chapter01/1.6 계산 단계와 포맷팅 단계 분리하기/index.js
+++ chapter01/1.6 계산 단계와 포맷팅 단계 분리하기/index.js
@@ -1,7 +1,11 @@
 const invoices = require('../json/invoices.json');
 const plays = require('../json/plays.json');
-const statement = require('./statement');
+const {
+    statement,
+    htmlStatement
+} = require('./statement');
 
 invoices.forEach((invoice) => {
     console.log(statement(invoice, plays));
+    console.log(htmlStatement(invoice, plays));
 });
