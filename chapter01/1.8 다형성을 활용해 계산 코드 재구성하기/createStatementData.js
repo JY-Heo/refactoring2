21ee5b1a4d3136a0128a613aa0524b3fed7c8138
JY Heo <jirung.h@gmail.com>
2020-05-24 19:25:16 +0900

    1.8/PerformanceCalculator에 amount() 메소드 추가

--- chapter01/1.8 다형성을 활용해 계산 코드 재구성하기/createStatementData.js
+++ chapter01/1.8 다형성을 활용해 계산 코드 재구성하기/createStatementData.js
@@ -4,6 +4,28 @@ class PerformanceCalculator {
         this.performance = aPerformance;
         this.play = aPlay;
     }
+
+    get amount() { // amountFor() 함수의 코드를 계산기 클래스로 복사
+        let result = 0;
+        switch (this.play.type) { // amountFor 함수가 매개변수로 받던 정보를 계산기 필드에서 바로 얻음
+            case 'tragedy':
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case 'comedy':
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${this.play.type}`);
+        }
+        return result; // 함수 안에서 값이 바뀌는 변수 반환
+    }
 }
 
 module.exports = function createStatementData(invoice, plays) { // 중간 데이터 생성을 전담
