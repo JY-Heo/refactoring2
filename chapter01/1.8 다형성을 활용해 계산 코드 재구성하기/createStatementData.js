1cce967e6f200ae53d3b4c5bb6e92d8ac115b4aa
JY Heo <jirung.h@gmail.com>
2020-05-24 19:35:31 +0900

    1.8/타입코드를 서브클래스로 바꾸기

--- chapter01/1.8 다형성을 활용해 계산 코드 재구성하기/createStatementData.js
+++ chapter01/1.8 다형성을 활용해 계산 코드 재구성하기/createStatementData.js
@@ -36,8 +36,21 @@ class PerformanceCalculator {
     }
 }
 
-function createPerformanceCalculator(aPerformance, play) {
-    return new PerformanceCalculator(aPerformance, play);
+class TragedyCalculator extends PerformanceCalculator {
+
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case 'tragedy': return new TragedyCalculator(aPerformance, aPlay);
+        case 'comedy': return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+    }
 }
 
 module.exports = function createStatementData(invoice, plays) { // 중간 데이터 생성을 전담
